f958628ddfbb005dac0c82acbc56de3081ca27f3
alirezajaberirad <alirezajaberirad@gmail.com>
2022-08-01 23:32:37 +0430

    Some corrections on CA1-phase1

--- CA1 - PacMan/Phase1 - Static Design Implementation/Header.h
+++ CA1 - PacMan/Phase1 - Static Design Implementation/Header.h
@@ -1,3 +1,3 @@
-void table(int length, int columns, int rows);
-void drawing(int r, char direction, float degree);
+void table(int length, int columns, int rows);//this function is used to display the playground
+void drawing(int r, char direction, float degree);//this function is used for drawing the pacman character
 #pragma once
